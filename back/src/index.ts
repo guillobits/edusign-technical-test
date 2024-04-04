f2f9aa619a9f071236205c1ee03237c8dcefc1c6
Guillaume CAUCHOIS <guillobits@gmail.com>
2024-04-04 12:43:57 +0200

    technical test

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import http from 'http';
+import fs from 'fs';
 
 const app = express();
 
@@ -10,8 +11,72 @@ app.use(express.urlencoded({ extended: true }));
 
 app.disable('x-powered-by');
 
-app.get('/', (req, res) => {
-	res.send("hello world");
+
+
+app.get('/students', (req, res) => {
+    try {
+        const fileContent = fs.readFileSync("../db.json", "utf-8");
+        const db = JSON.parse(fileContent);
+        const students = db.attendanceSheet?.STUDENTS;
+        res.json(students);
+    }
+    catch (err) {
+        res.status(500).end();
+    }
+});
+
+app.post('/students/:id/signature', (req, res) => {
+    const studentId = Number(req.params.id);
+
+    const signature = req.body.signature;
+
+    try {
+        const fileContent = fs.readFileSync("../db.json", "utf-8");
+        const db = JSON.parse(fileContent);
+
+        const idx = db.attendanceSheet?.STUDENTS?.findIndex((student : any) => {
+            return student.id === studentId;
+        });
+
+        if (idx < 0) {
+            res.status(404).end();
+        }
+        db.attendanceSheet.STUDENTS[idx].presenceState = true;
+        db.attendanceSheet.STUDENTS[idx].signatureTimestamp = Date.now();
+        db.attendanceSheet.STUDENTS[idx].signature = signature;
+
+        fs.writeFileSync("../db.json", JSON.stringify(db));
+        res.status(200).end();
+    }
+    catch (err) {
+        res.status(500).end();
+    }
+});
+
+app.delete('/students/:id/signature', (req, res) => {
+    const studentId = Number(req.params.id);
+
+    try {
+        const fileContent = fs.readFileSync("../db.json", "utf-8");
+        const db = JSON.parse(fileContent);
+
+        const idx = db.attendanceSheet?.STUDENTS?.findIndex((student : any) => {
+            return student.id === studentId;
+        });
+
+        if (idx < 0) {
+            res.status(404).end();
+        }
+        db.attendanceSheet.STUDENTS[idx].presenceState = null;
+        db.attendanceSheet.STUDENTS[idx].signatureTimestamp = null;
+        db.attendanceSheet.STUDENTS[idx].signature = null;
+
+        fs.writeFileSync("../db.json", JSON.stringify(db));
+        res.status(200).end();
+    }
+    catch (err) {
+        res.status(500).end();
+    }
 });
 
 const port = 3000;
